8b0a837227c9b780a9e0350aba8cc31cd42b60b4
HenryCodeT <120686@unsaac.edu.pe>
2022-02-14 12:50:02 -0500

    initial: practice

---
+++ JavaCoffee.java
@@ -0,0 +1,32 @@
+public class JavaCoffee {
+    public static void main(String[] args) {
+        // variables de la app
+        // lineas de texto de la app
+        String saludoGeneral = "Bienvenido al caje java, ";
+        String mensajePendiente = ", tu pedido esta listo en breve";
+        String mensajeListo = ", tu pedido esta listo";
+        String mensajeMostrarTotal = ", tu pedido es de $";
+
+        // variables de menu 
+        double precioMocca = 3.5;
+        double filterCoffee = 2;
+        double latte = 3;
+        double cappuccino = 6;
+
+        // variables de nombre de cliente
+        String cliente1 = "henry";
+        String cliente2 = "sam";
+        String cliente3 = "jimmy";
+        String cliente4 = "Noah";
+        
+        // Finalizaciones de pedidos 
+        boolean estaListoOrden1 = false ;
+        boolean estaListoOrden2 = true ;
+        boolean estaListoOrden3 = false ;
+        boolean estaListoOrden4 = true ;
+        
+        // SIMULACIÓN DE INTERACCIÓN DE APP(Agrega tu código para los desafíos a continuación)
+        // Ejemplo:
+        System.out.println(saludoGeneral+cliente1);
+    }
+}
